c858a36da8de569d9d0e6f3f83134dc8f0c8ec13
NadyaPetrova <nadsss@abv.bg>
2018-05-03 06:29:54 +0300

    Builder design pattern included

--- src/main/java/com/emag/model/Product.java
+++ src/main/java/com/emag/model/Product.java
@@ -8,48 +8,15 @@ public class Product {
 	private String brand;
 	private String model;
 	private String description;
-	private String productImageURL;
+	private String productPicture;
 	private double price;
 	private boolean availability;
 	private int discountPercent;
 	private Date discountExpiration;
+	private int availableQuantity;
 	private Category category = new Category();
 
-	
-	// ==============GETTERS=====================
-	
-	
-	public Product(String brand, String model, String description, String productImageURL, double price,
-			boolean availability, int discountPercent, Date discountExpiration) {
-		this.brand = brand;
-		this.model = model;
-		this.description = description;
-		this.productImageURL = productImageURL;
-		this.price = price;
-		this.availability = availability;
-		this.discountPercent = discountPercent;
-		this.discountExpiration = discountExpiration;
-	}
-
-	public Product(long productID, int categoryID, String brand, String model, String description,
-			String productImageURL, double price, boolean availability, int discountPercent, Date discountExpiration) {
-		this(brand, model, description, productImageURL, price, availability, discountPercent, discountExpiration);
-		this.productID = productID;
-		this.category.setCategoryID(categoryID);
-	}
-
-	public Product(long productID, String brand, String model, String description, String productImageURL, double price,
-			boolean availability, int discountPercent, Date discountExpiration, String categoryName) {
-		this(brand, model, description, productImageURL, price, availability, discountPercent, discountExpiration);
-		this.productID = productID;
-		this.category.setCategoryName(categoryName);
-	}
-	
-	public Product(int categoryID, String brand, String model, String description, String productImageURL, double price, boolean availability, int discountPercent, Date discountExpiration) {
-		this(brand, model, description, productImageURL, price, availability, discountPercent, discountExpiration);
-		this.category.setCategoryID(categoryID);
-	}
-
+	//==============GETTERS=====================
 	public String getModel() {
 		return model;
 	}
@@ -70,8 +37,8 @@ public class Product {
 		return description;
 	}
 
-	public String getProductImageURL() {
-		return productImageURL;
+	public String getProductPicture() {
+		return productPicture;
 	}
 
 	public boolean getAvailability() {
@@ -90,7 +57,90 @@ public class Product {
 		return category;
 	}
 	
+	//==========BUILDER DESIGN PATTERN==========
+	
+	public Product withProductID(long productID) {
+		this.productID = productID;
+		return this;
+	}
+
+	public Product withBrand(String brand) {
+		if (brand != null && !brand.isEmpty()) {
+			this.brand = brand;
+		}
+		return this;
+	}
+
+	public Product withModel(String model) {
+		if (model != null && !model.isEmpty()) {
+			this.model = model;
+		}
+		return this;
+	}
+
+	public Product withDescription(String description) {
+		this.description = description;
+		return this;
+	}
+
+	public Product withProductPicture(String productPicture) {
+		if (productPicture != null && !productPicture.isEmpty()) {
+			this.productPicture = productPicture;
+		}
+		return this;
+	}
+
+	public Product withPrice(double price) {
+		if (price >= 0) {
+			this.price = price;
+		}
+		return this;
+	}
+
+	public Product withAvailability(boolean availability) {
+		this.availability = availability;
+		return this;
+	}
+
+	public Product withDiscountPercent(int discountPercent) {
+		if (discountPercent >= 0) {
+			this.discountPercent = discountPercent;
+		}
+		return this;
+	}
+
+	public Product withDiscountExpiration(Date discountExpiration) {
+		this.discountExpiration = discountExpiration;
+		return this;
+	}
+
+	public Product withCategory(Category category) {
+		this.category = category;
+		return this;
+	}
+	
+	public Product withAvailableQuantity(int availableQuantity) {
+		this.availableQuantity = availableQuantity;
+		return this;
+	}
+	
+	public Product withCategoryName(String categoryName) {
+		this.category.setCategoryName(categoryName);
+		return this;
+	}
+	
+	public Product withCategoryID(int categoryID) {
+		this.category.setCategoryID(categoryID);
+		return this;
+	}
+	
+	//================SETTERS===================
+
 	public void setProductID(long productID) {
 		this.productID = productID;
+	}
+	
+	public void setProductPicture(String productPicture) {
+		this.productPicture = productPicture;
 	}
 }
